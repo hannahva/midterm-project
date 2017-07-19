cb63f5a6f03970d88888980886b460f949cb676f
bborncr <bentley@crcibernetica.com>
2017-07-19 23:04:54 +0000

    added lists and markers endpoints

--- routes/lists.js
+++ routes/lists.js
@@ -1,18 +1,89 @@
 "use strict";
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("users")
+      .from("lists")
       .then((results) => {
         res.json(results);
-    });
+      });
+  });
+
+  router.get("/:list_id", (req, res) => {
+    knex
+      .select("*")
+      .from("lists")
+      .where("id", list_id)
+      .then((results) => {
+        res.json(results);
+      });
+  });
+
+  // Create new list
+  router.post('/', (req, res) => {
+    // perform validations here
+    let list = [];
+    console.log(req.body);
+    if (true) {
+      list = [{
+        name: req.body.name,
+        description: req.body.description
+      }];
+    }
+    // Insert list into lists, returns the id as a confirmation
+    knex('lists')
+      .insert(list, 'id').then(function (id, err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Insert successful`);
+          res.send(`Insert successful\n`);
+        }
+      });
+  });
+
+  // Update list
+  router.put('/:list_id', (req, res) => {
+    // perform validation here
+    let list = [{
+      name: UNDEFINED,
+      description: UNDEFINED,
+    }];
+    console.log(req.body);
+    if (true) {
+      list = [{
+        name: req.body.name,
+        description: req.body.description
+      }];
+    }
+    knex('lists')
+      .where('id', list_id)
+      .update(list, 'id')
+      .then(function (id, err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Update successful`);
+          res.send(`Update successful\n`);
+        }
+      });
+  });
+
+  // Delete list
+  router.delete("/:list_id", (req, res) => {
+    knex("lists")
+      .where("id", list_id)
+      .del()
+      .then((results) => {
+        res.json(results);
+      });
   });
 
   return router;
+
 }
--- routes/markers.js
+++ routes/markers.js
@@ -4,15 +4,84 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (knex) => {
-
+  // GET all markers
   router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("users")
+      .from("markers")
       .then((results) => {
         res.json(results);
     });
   });
 
+  // GET specific marker
+  router.get("/:marker_id", (req, res) => {
+    knex
+      .select("*")
+      .from("markers")
+      .where("id", marker_id)
+      .then((results) => {
+        res.json(results);
+    });
+  });
+
+  // Create new marker
+  router.post('/', (req, res) => {
+    // perform validations here
+    let marker = [];
+    console.log(req.body);
+    if (true) {
+      marker = [{
+        list_id: req.body.list_id,
+        title: req.body.title,
+        description: req.body.description,
+        lat: req.body.lat,
+        lng: req.body.lng
+      }];
+    }
+    // Insert marker into markers, returns the id as a confirmation
+    knex('markers')
+      .insert(marker, 'id').then(function (id, err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Insert successful`);
+          res.send(`Insert successful\n`);
+        }
+      });
+  });
+
+  // Update marker
+  router.put('/:marker_id', (req, res) => {
+    // perform validation here
+    let marker = [{
+        name: UNDEFINED,
+        description: UNDEFINED,
+        lat: UNDEFINED,
+        lng: UNDEFINED
+      }];
+    console.log(req.body);
+    if (true) {
+      maker = [{
+        name: req.body.name,
+        description: req.body.description,
+        lat: req.body.lat,
+        lng: req.body.lng
+      }];
+    }
+    knex('markers')
+      .where('id', marker_id)
+      .update(marker, 'id')
+      .then(function (id, err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Update successful`);
+          res.send(`Update successful\n`);
+        }
+      });
+  });
+
   return router;
 }
+
