251b0a9fa38fe4d689a38cf410846e48b5595934
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-21 16:41:30 +0000

    working on bootstrap styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,14 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+  if (id){
+    $(".form-group.register").removeClass($(".col-sm-3");
+    $("div.main").removeClass($(".col-sm-6 .col-md-8")).addClass($(".col-sm-9 .col-md-10"));
+  }
 });
