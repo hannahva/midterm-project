9437be9918bff9671490deaa98556f8ef7eb2f03
Meredith <meredith.mumby@outlook.com>
2017-07-21 01:54:07 +0000

    Added flash-message

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,15 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+
+$("#submit-button-registration").submit(function( event) {
+   event.preventDefault();
+  console.log("its not working");
+    if(!$(this).val() && $(this).val() !== "") {
+      console.log("its working");
+      $.flash('OOps! Invalid information. Try Again');
+    } else if (!$(this).val() && $(this).val() !== "") {
+      $.flash('OOps! Invalid information. Try Again');
     }
-  });;
 });
+
+});
+
